5138bd44b9369f32f28beccca802de4eb98736c6
Theo Le Moal <contact@theo-le-moal.fr>
2023-05-15 12:49:49 +0200

    tp-02 fini

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -1,7 +1,18 @@
 package fr.declaration.variable;
 
-public class Main {
+public class DeclarationApp {
     public static void main(String[] args) {
-
+        byte byteNumber = 10;
+        short shortNumber = -17282;
+        int intNumber = 2846325;
+        long longNumber = 10000L;
+        float floatNumber = 2.5F;
+        double doubleNumber = 6.4D;
+        char charText = 't';
+        boolean bool = true;
+        String text = "Ceci est un String";
+        System.out.println("text = " + text);
+        String randomString = "Voici le résultat d’un calcul : \n1+5=6";
+        System.out.println("randomString = " + randomString);
     }
 }

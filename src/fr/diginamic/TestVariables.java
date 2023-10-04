1b98d3fc6ce4d4a61bb79b2f3c0f310f63997e07
Theo <contact@theo-le-moal.fr>
2023-10-04 13:57:27 +0200

    first commit

---
+++ src/fr/diginamic/TestVariables.java
@@ -0,0 +1,17 @@
+package fr.diginamic;
+
+public class TestVariables {
+	public static void main(String[] args) {
+		int populationFrance = 66;
+		String nom = "Le Moal";
+		double poids = 79.5;
+		boolean test = true;
+		char c = 'a';
+		System.out.println(populationFrance);
+		System.out.println(nom);
+		System.out.println(poids);
+		System.out.println(test);
+		System.out.println(c);
+	}
+}
+ 

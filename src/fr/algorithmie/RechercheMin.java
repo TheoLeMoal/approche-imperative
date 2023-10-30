6f844674c2c76d4481eb31a891385a7d80740108
Theo <contact@theo-le-moal.fr>
2023-10-30 14:31:55 +0100

    Projet terminé

--- src/fr/algorithmie/RechercheMin.java
+++ src/fr/algorithmie/RechercheMin.java
@@ -4,15 +4,18 @@ public class RechercheMin {
 
 	public static void main(String[] args) {
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+		
 		/**
 		 * Rechercher le plus petit élément du tableau
 		 */
+		
 		int minValue  = 0;
 		for (int i : array) {
 			if (minValue > i) {
 				minValue = i;
 			}
 		}
+		
 		System.out.println(minValue);
 	}
 
